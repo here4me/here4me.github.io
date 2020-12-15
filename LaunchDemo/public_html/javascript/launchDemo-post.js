d1176c2e45eb8f824764a0a804999640ca66ab15
chrisehlen <christopher_ehlen@yahoo.com>
2020-12-15 14:12:53 -0700

    Added callback to check step one post exists.

--- LaunchDemo/public_html/javascript/launchDemo-post.js
+++ LaunchDemo/public_html/javascript/launchDemo-post.js
@@ -2,31 +2,34 @@
 
 here4Me.addEventListener('initialize', function () {
 
-    if (!qrCodePostExists()) {
-        
-        createDemoPost();
-    }
+    checkIfStepOnePostExists(function (exists) {
+
+        if (!exists) {
+
+            createDemoPost();
+        }
+    });
 });
 
-function qrCodePostExists() {
+function checkIfStepOnePostExists(callback) {
 
     here4Me.readAllPosts(function (response) {
 
         if (response.statusCode !== 'SUCCESSFUL') {
-            
-            return false;
+
+            callback(false);
         }
 
         let posts = response.message;
         for (var i = 0; i < posts.length; i++) {
 
             if (posts[i].content === 'LAUNCH_DEMO_STEP_ONE') {
-                
-                return true;
+
+                callback(true);
             }
         }
 
-        return false;
+        callback(false);
     });
 }
 
