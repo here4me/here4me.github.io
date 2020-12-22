953ee6ec8f56bac5ad6e11f08bdd085352395cdd
chrisehlen <christopher_ehlen@yahoo.com>
2020-12-21 17:04:49 -0700

    Add site not active.

--- LaunchDemo/public_html/javascript/here4me-client.js
+++ LaunchDemo/public_html/javascript/here4me-client.js
@@ -844,7 +844,7 @@ function initializeHere4me() {
                     let message = {
                         userId: data.userId,
                         userIsAnonymous: data.message.userIsAnonymous,
-                        siteIsActive: data.message.activeHome !== null
+                        siteIsActive: data.message.activeHome
                     };
                     here4Me.initializeEventListeners[i](message);
                 }
