b8bc96cbc6ccb1ae7552b4d0aff785c90f4daf51
chrisehlen <christopher_ehlen@yahoo.com>
2020-12-26 14:45:47 -0700

    Add configuration view

--- LaunchDemo/public_html/javascript/launchDemo-configuration.js
+++ LaunchDemo/public_html/javascript/launchDemo-configuration.js
@@ -1,2 +1,8 @@
+/* global here4Me */
 
+here4Me.addEventListener('initialize', function (message) {
 
+    here4Me.enableConfigurationButton();
+    document.body.style.display = 'block';
+    here4Me.resize();
+});
