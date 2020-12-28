2a8a28fea2a56908235fe3905ead3f2b8cd65ce5
chrisehlen <christopher_ehlen@yahoo.com>
2020-12-28 11:38:14 -0700

    Add configuration view

--- LaunchDemo/public_html/javascript/launchDemo-configuration.js
+++ LaunchDemo/public_html/javascript/launchDemo-configuration.js
@@ -1,8 +1,106 @@
 /* global here4Me */
 
+const CONTENT_TYPE = 'demo-data';
+const SITE_ID = '63f58ae9fcdae20ea8d3743ab806ab2f';
+const SITE_OWNER_ID = '8ae1a3cf2fa609656eaa447f8fe99b15';
+
+let siteQRCodeMessageElement = document.getElementById('siteQRCodeMessage');
+let setQRCodeButtonElement = document.getElementById('setQRCodeButton');
+
 here4Me.addEventListener('initialize', function (message) {
 
     here4Me.enableConfigurationButton();
     document.body.style.display = 'block';
     here4Me.resize();
 });
+
+siteQRCodeMessageElement.onkeyup = function () {
+
+    if (siteQRCodeMessageElement.value.trim() === '') {
+
+        setQRCodeButtonElement.setAttribute('disabled', true);
+    } else {
+
+        setQRCodeButtonElement.removeAttribute('disabled');
+    }
+};
+
+setQRCodeButtonElement.onclick = function () {
+
+    getUserQRCodeContent(function (qrCodeContent) {
+
+        if (qrCodeContent !== null) {
+
+            here4Me.deleteQRCodeContent(qrCodeContent, function (response) {});
+        }
+        setUserQRCodeContent();
+    });
+};
+
+function getUserQRCodeContent(callback) {
+
+    here4Me.getUserQRCodeContentId(function (response) {
+
+        let qrCodeContentId = response.message;
+        if (response.statusCode === 'SUCCESSFUL' && qrCodeContentId !== null) {
+
+            here4Me.readQRCodeContent(response.message, function (response) {
+
+                let qrCodeContent = response.message;
+                if (response.statusCode === 'SUCCESSFUL') {
+
+                    callback(qrCodeContent);
+                    return;
+                }
+                callback(null);
+            });
+            return;
+        }
+        callback(null);
+    });
+}
+
+function setUserQRCodeContent() {
+
+    createQRCodeContent(buildQRCodeContent(), function (qrCodeContentId) {
+
+        if (qrCodeContentId !== null) {
+
+            here4Me.setUserQRCodeContentId(qrCodeContentId, function (response) {
+                
+                here4Me.disableConfigurationButton();
+                here4Me.enableScanButton();
+            });
+        }
+    });
+}
+
+function createQRCodeContent(qrCodeContent, callback) {
+
+    here4Me.createQRCodeContent(qrCodeContent, function (response) {
+
+        let qrCodeContentId = response.message.id;
+        if (response.statusCode === 'SUCCESSFUL') {
+
+            callback(qrCodeContentId);
+        }
+        callback(null);
+    });
+}
+
+function buildQRCodeContent() {
+
+    let qrCodeContent = {
+        id: null,
+        acceptedSiteIds: [SITE_ID],
+        acceptedSiteOwnerIds: [SITE_OWNER_ID],
+        contentType: CONTENT_TYPE,
+        filter: null,
+        content: siteQRCodeMessageElement.value,
+        service: null,
+        context: null,
+        version: null
+    };
+
+    return qrCodeContent;
+}
