f09c12f32ca8fc18dee4eb24ee151ed3da6ffdad
chrisehlen <christopher_ehlen@yahoo.com>
2021-01-02 15:42:17 -0700

    Add markdown example

--- MarkdownExample/public_html/javascript/markdownExample-home.js
+++ MarkdownExample/public_html/javascript/markdownExample-home.js
@@ -2,5 +2,17 @@
 
 here4Me.addEventListener('initialize', function (message) {
 
-    let mySwipe = Swipe(document.getElementById('slider'));
+    let mySwipe = Swipe(document.getElementById('slider'), {
+        startSlide: 2,
+        speed: 400,
+        auto: 3000,
+        continuous: true,
+        disableScroll: false,
+        stopPropagation: false,
+        callback: function (index, elem) {},
+        transitionEnd: function (index, elem) {
+            here4Me.scrollTo(0, 0);
+            here4Me.resize();
+        }
+    });
 });
