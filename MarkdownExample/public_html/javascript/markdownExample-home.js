76da964b10cfd5484c19f952ab1c1ef55b7bdc92
chrisehlen <christopher_ehlen@yahoo.com>
2021-01-02 15:48:26 -0700

    Add markdown example

--- MarkdownExample/public_html/javascript/markdownExample-home.js
+++ MarkdownExample/public_html/javascript/markdownExample-home.js
@@ -3,12 +3,6 @@
 here4Me.addEventListener('initialize', function (message) {
 
     let mySwipe = Swipe(document.getElementById('slider'), {
-        startSlide: 1,
-        speed: 400,
-        auto: 3000,
-        continuous: true,
-        disableScroll: false,
-        stopPropagation: false,
         callback: function (index, elem) {},
         transitionEnd: function (index, elem) {
             here4Me.scrollTo(0, 0);
