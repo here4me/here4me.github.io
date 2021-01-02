bf42df43ea99ee2c11d13c308d665f54d904193e
chrisehlen <christopher_ehlen@yahoo.com>
2021-01-02 14:04:55 -0700

    Add markdown example

---
+++ MarkdownExample/public_html/javascript/markdownExample-home.js
@@ -0,0 +1,16 @@
+    var swiperH = new Swiper('.swiper-container-h', {
+      spaceBetween: 50,
+      pagination: {
+        el: '.swiper-pagination-h',
+        clickable: true
+      }
+    });
+    
+    var swiperV = new Swiper('.swiper-container-v', {
+      direction: 'vertical',
+      spaceBetween: 50,
+      pagination: {
+        el: '.swiper-pagination-v',
+        clickable: true
+      }
+    });
